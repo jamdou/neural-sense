03c7da16b727c3ddae3abb6f9c80e1d4501460b2
Alexander Tritt (Monash) <alexander.tritt@monash.edu>
2020-10-06 12:50:52 +1100

    Removed my personal directory for profile.bat

--- profile.bat
+++ profile.bat
@@ -1,5 +1,7 @@
 @ECHO OFF
 REM Run GPU profiles to work with nvvp
-nvprof --quiet --export-profile C:\Users\Alex\University\NeuralSense\profile\timeline.prof python C:\Users\Alex\University\NeuralSense\main.py --profile=timeline
-nvprof --quiet --metrics all --events all -o C:\Users\Alex\University\NeuralSense\profile\metricsEvents.prof python C:\Users\Alex\University\NeuralSense\main.py --profile=metric
-python C:\Users\Alex\University\NeuralSense\main.py --profile=archive
+RMDIR .\profile /s /q
+MKDIR .\profile
+nvprof --quiet --export-profile .\profile\timeline.prof python .\main.py --profile=timeline
+nvprof --quiet --metrics all --events all -o .\profile\metricsEvents.prof python .\main.py --profile=metric
+python .\main.py --profile=archive
