3fa795b85caf65d9b850960890e5ab813c619210
Alexander Tritt (Monash) <alexander.tritt@monash.edu>
2020-11-04 14:24:25 +1100

    Profiling with name changes

--- profile.bat
+++ profile.bat
@@ -6,16 +6,16 @@ RMDIR .\profile /s /q
 MKDIR .\profile
 
 REM Run at full speed to produce a timeline
-nvprof --quiet --export-profile .\profile\timeline.prof python .\main.py --profile=timeline
+nvprof --quiet --export-profile .\profile\timeline.prof python . --profile=timeline
 
 REM Run instruction level analysis
 REM To run instruction level tests that also link to source code, you must enable debug mode ONLY for the kernel
 REM that you want to test (nvidia only allows one for some reason). Be aware that this slows the execution of the
 REM kernel
-nvprof --quiet --source-level-analysis instruction_execution --export-profile .\profile\instructionLevel.prof python .\main.py --profile=instructionlevel
+nvprof --quiet --source-level-analysis instruction_execution --export-profile .\profile\instructionLevel.prof python . --profile=instructionlevel
 
 REM Run slowly and collects metrics for the smart report in nvvp
-nvprof --quiet --metrics all --events all -o .\profile\metricsEvents.prof python .\main.py --profile=metric
+nvprof --quiet --metrics all --events all -o .\profile\metricsEvents.prof python . --profile=metric
 
 REM Archive the generated profiles
-python .\main.py --profile=archive
+python . --profile=archive
